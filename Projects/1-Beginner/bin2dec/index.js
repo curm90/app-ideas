3ea4138721df9f57c98125b627c45c3735299ac6
Liam Sutton <devliam90@gmail.com>
2020-06-15 12:24:18 +0100

    form validation

--- Projects/1-Beginner/bin2dec/index.js
+++ Projects/1-Beginner/bin2dec/index.js
@@ -2,11 +2,18 @@ const userInput = document.querySelector('.user__input');
 const form = document.querySelector('.form');
 const answer = document.querySelector('.answer');
 
+const regExp = /^[0-1]+$/g;
+
 const bin2Dec = (number) => {
   return parseInt(number, 2);
 };
 
 const formValidation = (input) => {
+  if (input.value.match(regExp) === null) {
+    answer.textContent = 'Please enter either 0 or 1';
+    return;
+  }
+
   if (input.value.length > 8) {
     answer.textContent =
       'Binary number too long. Please enter a binary number up to 8 digits.';
